ec59ce06596bf52f8afff96ad456002a86ecacb9
kuro_san@outlook.es <a3519110051@alumno.uttehuacan.edu.mx>
2022-09-22 19:43:11 -0500

    vue3-contador 10A

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,38 @@ export default {
 };
 </script>
 <template>
+  <nav class="navbar navbar-expand-lg bg-light fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Oswaldo Vega González 10 A</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <img src="./assets/logo.svg" alt="Logo VUE" width="125" height="125" />
   <div class="container">
     <small>
@@ -36,6 +68,11 @@ export default {
 <style scoped>
 .container {
   text-align: center;
+  margin-top: 150px;
+}
+img{
+  margin-top: 150px;
+  margin-left: 250px;
 }
 small {
   color: #666;
